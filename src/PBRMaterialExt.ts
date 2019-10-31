ebef4ac73e0bdd742b68d2244b2010db28c1e2f5
Michael Grau <michael.grau@kurzdigital.com>
2019-10-31 09:34:30 +0100

    Adjust ext material

--- src/PBRMaterialExt.ts
+++ src/PBRMaterialExt.ts
@@ -1,5 +1,4 @@
-import { Scene, Effect, BaseTexture, expandToProperty, Mesh, Texture, MaterialHelper, AbstractMesh, PBRMaterialDefines, PBRMaterial, serializeAsTexture } from '@babylonjs/core';
-import { PBRCustomMaterial } from '@babylonjs/materials/custom/pbrCustomMaterial';
+import { Scene, Effect, BaseTexture, expandToProperty, Mesh, PBRMaterialDefines, PBRMaterial, serializeAsTexture } from '@babylonjs/core';
 import { Nullable } from '@babylonjs/core/types';
 import fragCustomMat from './shaderparts/fragCustomMat.frag';
 
@@ -11,12 +10,30 @@ export class PBRMaterialExt extends PBRMaterial {
     samplers: string[] = [];
     static _matIndex: number = -1;
 
+     // detail normal 1
+     private _detailTexture1: Nullable<BaseTexture> = null;
+    
+     @serializeAsTexture()
+     @expandToProperty('_markAllSubMeshesAsTexturesDirty')
+     public detailTexture1: BaseTexture;
+ 
+     // detail normal 2
+     private _detailTexture2: Nullable<BaseTexture> = null;
+     
+     @serializeAsTexture()
+     @expandToProperty('_markAllSubMeshesAsTexturesDirty')
+     public detailTexture2: BaseTexture;
+
     constructor(name: string, scene: Scene) {
         super(name, scene);
+        // get base shader
         this.FragmentShader = Effect.ShadersStore['pbrPixelShader'];
         this.VertexShader = Effect.ShadersStore['pbrVertexShader'];
+        // replace shader part
         const fragmentSearch = "gl_FragColor=finalColor;"
         this.FragmentShader = this.FragmentShader.replace(fragmentSearch, fragmentSearch + '\n' + fragCustomMat);
+
+        // add custom name resolve that is executed when something is changed
         this.customShaderNameResolve = this.NameResolve;
     }
 
@@ -24,26 +41,29 @@ export class PBRMaterialExt extends PBRMaterial {
     protected NameResolve(shaderName: string, uniforms: string[], uniformBuffers: string[], samplers: string[], defines: PBRMaterialDefines) {
         
         if(!this.customName) {
+            // count up name index for every new material
             PBRMaterialExt._matIndex++;
             this.customName = "pbrExt" + PBRMaterialExt._matIndex;
         }
-        
-        console.log("NameResolve", this.customName, samplers, uniforms);
-        
-        this.markDirty();
-        
-        this.prepare(defines, samplers);
-        samplers.push(... this.samplers);
                 
+        // prepare new data based on set variables
+        this.prepare(defines, samplers);
+        // add own samplers if not already in for effect caching
+        this.samplers.filter(s => !samplers.includes(s)).forEach(s => samplers.push(s));
+
+        // add custom uniforms to shader
         const fragUnisReplacement = this.uniforms.map(uni => "uniform " + uni + ";\n").join();
+        // add custom samplers to shader
         const fragSampReplacement = this.samplers.map(samp => "uniform sampler2D " + samp + ";\n").join();
 
 
+        // set shaders in shaderstore
         Effect.ShadersStore[this.customName + "VertexShader"] = this.VertexShader;
         Effect.ShadersStore[this.customName + "PixelShader"] = this.FragmentShader
             .replace('#define CUSTOM_FRAGMENT_DEFINITIONS', fragSampReplacement + fragUnisReplacement)
 
 
+        // attach afterBind for setting the uniforms and textures in the effect
         const oldAfterBind = this._afterBind.bind(this);
         this._afterBind = (m, e) => {
             if (!e) {
@@ -61,8 +81,8 @@ export class PBRMaterialExt extends PBRMaterial {
         return this.customName;
     }
 
+    // set data inside the effect after bind
     protected AttachAfterBind(mesh: Mesh, effect: Effect) {
-            // console.log("AfterBind");
             if (effect.defines.includes("DETAIL1") && this._detailTexture1 && this.detailTexture1.isReady()) {
                 effect.setTexture("detail1Sampler", this._detailTexture1);
             }
@@ -73,40 +93,27 @@ export class PBRMaterialExt extends PBRMaterial {
 
     
     protected prepare(defines: PBRMaterialDefines, samplers: string[]) {
+        // regather information about used uniforms and textures
         this.uniforms = [];
         this.samplers = [];
-        samplers = samplers.filter( s => s !== "detail1Sampler" && s !== "detail2Sampler");
         if (this._detailTexture1 && this._detailTexture1 !== undefined) {
             defines.DETAIL1 = true;
-            samplers.push("detail1Sampler");
             this.samplers.push("detail1Sampler");
         } else {
             delete defines.DETAIL1;
         }
+
         if (this._detailTexture2) {
             defines.DETAIL2 = true;
-            samplers.push("detail2Sampler");
             this.samplers.push("detail2Sampler");
         } else {
             delete defines.DETAIL2;
         }
+
+        // rebuild the defines so the shader is recompiled
         defines.rebuild();
         
     }
-
-    // detail normal
-    private _detailTexture1: Nullable<BaseTexture> = null;
-    
-    @serializeAsTexture()
-    @expandToProperty('_markAllSubMeshesAsTexturesDirty')
-    public detailTexture1: BaseTexture;
-
-    // detail normal
-    private _detailTexture2: Nullable<BaseTexture> = null;
-    
-    @serializeAsTexture()
-    @expandToProperty('_markAllSubMeshesAsTexturesDirty')
-    public detailTexture2: BaseTexture;
 
 
 }
